a82d17977dc03ea44fc16144f6b43649172c142b
HumaCode <humaidizakaria1998@gmail.com>
2026-07-20 14:56:08 +0700

    fix(role): disable non-existent permissions to prevent validation error when updating dev role

--- resources/views/pages/role-management/roles/partials/form-akses-view.blade.php
+++ resources/views/pages/role-management/roles/partials/form-akses-view.blade.php
@@ -77,14 +77,14 @@
                             $pActivate = "activate {$permKey}";
                             $pMenu = "menu {$permKey}";
 
-                            // Jika DEV, paksa TRUE agar tidak di-disable. Jika bukan, cek dari database.
-                            $isReadAvail = $isDev || in_array($pRead, $allSystemPermissions);
-                            $isShowAvail = $isDev || in_array($pShow, $allSystemPermissions);
-                            $isCreateAvail = $isDev || in_array($pCreate, $allSystemPermissions);
-                            $isUpdateAvail = $isDev || in_array($pUpdate, $allSystemPermissions);
-                            $isDeleteAvail = $isDev || in_array($pDelete, $allSystemPermissions);
-                            $isActivateAvail = $isDev || in_array($pActivate, $allSystemPermissions);
-                            $isMenuAvail = $isDev || in_array($pMenu, $allSystemPermissions);
+                            // Cek ketersediaan permission di database (harus terdaftar di tabel permissions)
+                            $isReadAvail = in_array($pRead, $allSystemPermissions);
+                            $isShowAvail = in_array($pShow, $allSystemPermissions);
+                            $isCreateAvail = in_array($pCreate, $allSystemPermissions);
+                            $isUpdateAvail = in_array($pUpdate, $allSystemPermissions);
+                            $isDeleteAvail = in_array($pDelete, $allSystemPermissions);
+                            $isActivateAvail = in_array($pActivate, $allSystemPermissions);
+                            $isMenuAvail = in_array($pMenu, $allSystemPermissions);
                         @endphp
 
                         <tr class="item-row" data-grp="{{ $grpSlug }}">
@@ -95,7 +95,7 @@
                                 <label class="sw-wrap sw-c" title="Read"
                                     style="{{ !$isReadAvail ? 'opacity: 0.3; cursor: not-allowed;' : '' }}">
                                     <input type="checkbox" name="permissions[]" value="{{ $pRead }}"
-                                        id="sw_{{ $permKey }}_read" @checked($isDev || ($isReadAvail && $data->hasPermissionTo($pRead)))
+                                        id="sw_{{ $permKey }}_read" @checked($isReadAvail && ($isDev || $data->hasPermissionTo($pRead)))
                                         @disabled(!$isReadAvail)>
                                     <span class="sw-track"></span>
                                 </label>
@@ -106,7 +106,7 @@
                                 <label class="sw-wrap sw-c" title="Show / Detail"
                                     style="{{ !$isShowAvail ? 'opacity: 0.3; cursor: not-allowed;' : '' }}">
                                     <input type="checkbox" name="permissions[]" value="{{ $pShow }}"
-                                        id="sw_{{ $permKey }}_show" @checked($isDev || ($isShowAvail && $data->hasPermissionTo($pShow)))
+                                        id="sw_{{ $permKey }}_show" @checked($isShowAvail && ($isDev || $data->hasPermissionTo($pShow)))
                                         @disabled(!$isShowAvail)>
                                     <span class="sw-track"></span>
                                 </label>
@@ -117,7 +117,7 @@
                                 <label class="sw-wrap sw-g" title="Create"
                                     style="{{ !$isCreateAvail ? 'opacity: 0.3; cursor: not-allowed;' : '' }}">
                                     <input type="checkbox" name="permissions[]" value="{{ $pCreate }}"
-                                        id="sw_{{ $permKey }}_create" @checked($isDev || ($isCreateAvail && $data->hasPermissionTo($pCreate)))
+                                        id="sw_{{ $permKey }}_create" @checked($isCreateAvail && ($isDev || $data->hasPermissionTo($pCreate)))
                                         @disabled(!$isCreateAvail)>
                                     <span class="sw-track"></span>
                                 </label>
@@ -128,7 +128,7 @@
                                 <label class="sw-wrap sw-a" title="Update"
                                     style="{{ !$isUpdateAvail ? 'opacity: 0.3; cursor: not-allowed;' : '' }}">
                                     <input type="checkbox" name="permissions[]" value="{{ $pUpdate }}"
-                                        id="sw_{{ $permKey }}_update" @checked($isDev || ($isUpdateAvail && $data->hasPermissionTo($pUpdate)))
+                                        id="sw_{{ $permKey }}_update" @checked($isUpdateAvail && ($isDev || $data->hasPermissionTo($pUpdate)))
                                         @disabled(!$isUpdateAvail)>
                                     <span class="sw-track"></span>
                                 </label>
@@ -139,7 +139,7 @@
                                 <label class="sw-wrap sw-r" title="Delete"
                                     style="{{ !$isDeleteAvail ? 'opacity: 0.3; cursor: not-allowed;' : '' }}">
                                     <input type="checkbox" name="permissions[]" value="{{ $pDelete }}"
-                                        id="sw_{{ $permKey }}_delete" @checked($isDev || ($isDeleteAvail && $data->hasPermissionTo($pDelete)))
+                                        id="sw_{{ $permKey }}_delete" @checked($isDeleteAvail && ($isDev || $data->hasPermissionTo($pDelete)))
                                         @disabled(!$isDeleteAvail)>
                                     <span class="sw-track"></span>
                                 </label>
@@ -150,7 +150,7 @@
                                 <label class="sw-wrap sw-v" title="Activate"
                                     style="{{ !$isActivateAvail ? 'opacity: 0.3; cursor: not-allowed;' : '' }}">
                                     <input type="checkbox" name="permissions[]" value="{{ $pActivate }}"
-                                        id="sw_{{ $permKey }}_activate" @checked($isDev || ($isActivateAvail && $data->hasPermissionTo($pActivate)))
+                                        id="sw_{{ $permKey }}_activate" @checked($isActivateAvail && ($isDev || $data->hasPermissionTo($pActivate)))
                                         @disabled(!$isActivateAvail)>
                                     <span class="sw-track"></span>
                                 </label>
@@ -161,7 +161,7 @@
                                 <label class="sw-wrap sw-p" title="Menu"
                                     style="{{ !$isMenuAvail ? 'opacity: 0.3; cursor: not-allowed;' : '' }}">
                                     <input type="checkbox" name="permissions[]" value="{{ $pMenu }}"
-                                        id="sw_{{ $permKey }}_menu" @checked($isDev || ($isMenuAvail && $data->hasPermissionTo($pMenu)))
+                                        id="sw_{{ $permKey }}_menu" @checked($isMenuAvail && ($isDev || $data->hasPermissionTo($pMenu)))
                                         @disabled(!$isMenuAvail)>
                                     <span class="sw-track"></span>
                                 </label>
